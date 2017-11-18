190edb204a7ea890d2e5f177904c243d7df87f19
Renat Kaitmazov <renatkaitmazov@gmail.com>
2017-11-18 17:37:21 +0300

    Add a symbol table interface

---
+++ src/main/java/lz/renatkaitmazov/algorithms/week4/SymbolTable.java
@@ -0,0 +1,82 @@
+package lz.renatkaitmazov.algorithms.week4;
+
+/**
+ * An abstraction of the data type known as a symbol table which stores values
+ * associated with keys. Sometimes it is called a dictionary.
+ * Implementations of this interface do not use hashing so the operations of
+ * inserting, deleting, and searching are slow.
+ * Hashing will be covered on week 6.
+ *
+ * @author Renat Kaitmazov
+ */
+
+public interface SymbolTable<K extends Comparable<K>, V> {
+
+    // TODO 2) Add tests for its concrete implementations
+    // TODO 3) Add concrete implementations
+
+    /**
+     * Returns the amount of items in the queue.
+     *
+     * @return number of items.
+     */
+    int size();
+
+    /**
+     * Checks to see if the queue is empty or not.
+     *
+     * @return <code>true</code> if there are no items in the queue,
+     * <code>false</code> otherwise.
+     */
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Puts the given value paired with the key into the table.
+     * If the value is <code>null</code>, removes the key from the table.
+     * Throws a runtime exception if the key is <code>null</code>.
+     * If the key is already in the table, it updates paired with the key.
+     *
+     * @param key   a key
+     * @param value associated with the key
+     */
+    void put(K key, V value);
+
+    /**
+     * Removes a pair of the given key and its associated value.
+     *
+     * @param key to remove.
+     * @return value, paired with the key,
+     * <code>null</code> if the key was absent.
+     */
+    V remove(K key);
+
+    /**
+     * Returns a value associated with the given key.
+     *
+     * @param key whose value should be returned.
+     * @return value, paired with the key,
+     * <code>null</code> if the key was absent.
+     */
+    V get(K key);
+
+    /**
+     * Checks to see if the given key is present in the table.
+     *
+     * @param key to check.
+     * @return <code>true</code> if the key is in the table,
+     * <code>false</code> otherwise.
+     */
+    default boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    /**
+     * Returns an iterable collection of keys.
+     *
+     * @return a collection of keys,
+     * <code>null</code> if the table is empty.
+     */
+    Iterable<K> keys();
+}
